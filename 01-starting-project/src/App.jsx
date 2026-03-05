3e70e5629f5577ddbf7651c0b954c96628fb8c0d
Sarath-33 <withsarath.m@gmail.com>
2026-03-05 23:34:09 +0530

    Add TabButton component and implement topic selection in App component

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,8 +1,15 @@
 import { CORE_CONCEPTS } from "./data.js";
 import Header from "./components/header/Header.jsx";
 import CoreConcepts from "./components/CoreConcepts.jsx";
+import TabButton from "./components/TabButton.jsx";
+import { useState } from "react";
 
 function App() {
+  const [selectedTopic, setSelectedTopic]= useState("Please Click One of the Button Above..")
+  function handleSelect(selectedButton) {
+    setSelectedTopic(selectedButton)
+    console.log(selectedButton);
+  }
   return (
     <div>
       <Header />
@@ -15,6 +22,16 @@ function App() {
             <CoreConcepts {...CORE_CONCEPTS[2]} />
             <CoreConcepts {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={()=>handleSelect("Components")}>Components</TabButton>
+            <TabButton onSelect={()=>handleSelect("Jsx")}>Jsx</TabButton>
+            <TabButton onSelect={()=>handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={()=>handleSelect("State")}>State</TabButton>
+          </menu>
+         {selectedTopic}
         </section>
       </main>
     </div>
