fe4268b846b7036ee4715fa7275fbafcae776cb2
Sarath-33 <withsarath.m@gmail.com>
2026-03-13 19:39:00 +0530

    feat: implement investment calculator components and logic - Add App component to manage state and render child components - Create Header, UserInput, and Results components for user interaction and displaying results - Implement input validation and investment calculation logic

--- investment-project/src/App.jsx
+++ investment-project/src/App.jsx
@@ -1,7 +1,34 @@
+import { useState } from "react";
+import Header from "./components/Header";
+import Results from "./components/Results";
+import UserInput from "./components/UserInput";
+
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  const inputIsValid = userInput.duration >= 1;
+
+  function handleChange(inputIdentifer, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifer]: +newValue,
+      };
+    });
+  }
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <>
+      <Header />
+      <UserInput onChange={handleChange} userInput={userInput} />
+      {!inputIsValid && <p className="center">Please enter a duration greater than zero.</p> }
+      {inputIsValid && <Results input={userInput} />}
+    </>
+  );
 }
 
-export default App
+export default App;
