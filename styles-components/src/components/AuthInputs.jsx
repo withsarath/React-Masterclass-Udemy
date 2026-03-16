c7db55502b3aa3b73143e8a63aa77a17ac606adb
Sarath-33 <withsarath.m@gmail.com>
2026-03-16 10:58:06 +0530

    refactor: replace styled-components with Tailwind CSS for consistent styling

--- styles-components/src/components/AuthInputs.jsx
+++ styles-components/src/components/AuthInputs.jsx
@@ -1,14 +1,6 @@
 import { useState } from "react";
-import { styled } from "styled-components";
 import Button from "./Button";
 import Input from "./Input";
-
-const ControlledDiv = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  margin-bottom: 1.5rem;
-`;
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState("");
@@ -31,25 +23,27 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <ControlledDiv>
+    <div
+      id="auth-inputs"
+      className="w-full max-w-sm mx-auto p-8 rounded shadow-md bg-linear-to-b from-stone-700 to-stone-800"
+    >
+      <div className="flex flex-col gap-2 mb-6">
         <Input
           label="Email"
-          invaild={emailNotValid}
+          $invalid={emailNotValid}
           type="email"
           onChange={(event) => handleInputChange("email", event.target.value)}
         />
-
         <Input
-          invalid={passwordNotValid}
+          $invalid={passwordNotValid}
           label="password"
           type="password"
           onChange={(event) =>
             handleInputChange("password", event.target.value)
           }
         />
-      </ControlledDiv>
-      <div className="actions">
+      </div>
+      <div className="flex justify-end gap-4 text-amber-400 hover: text-amber-500">
         <button type="button" className="text-button">
           Create a new account
         </button>
