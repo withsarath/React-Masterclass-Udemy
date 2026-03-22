3c09349a4e969ad262facf8548e8b881f6a8f145
Sarath-33 <withsarath.m@gmail.com>
2026-03-22 11:28:18 +0530

    inital project

---
+++ Project-Making/src/App.jsx
@@ -0,0 +1,11 @@
+import ProjectSidebar from "./components/ProjectsSidebar";
+
+function App() {
+  return (
+    <main className="h-screen my-8">
+      <ProjectSidebar/>
+    </main>
+  );
+}
+
+export default App;
